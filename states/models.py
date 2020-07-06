3eb54da9a9c9ba1705ba0ec2c6d68a07503d5947
Enyioma Osondu <o.enyioma@gmail.com>
2020-07-06 23:21:16 +0100

    redid migrations

--- states/models.py
+++ states/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 
 class State(models.Model):
-    name = models.CharField(max_length=20)
-    capital = models.CharField(max_length=20)
-    slug = models.SlugField(max_length=21)
+    name = models.CharField(max_length=32)
+    capital = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=32)
 
     def __str__(self):
         return self.name
@@ -14,7 +14,7 @@ class State(models.Model):
 class LocalGovernmentArea(models.Model):
     state = models.ForeignKey(
         State, on_delete=models.SET_NULL, null=True, related_name='lgas')
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=32)
 
     def __str__(self):
         return self.name
